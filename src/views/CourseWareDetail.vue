012ffb04d21c0a175c765b4a738b635dd2e4ed39
wangyalijas <1114920411@qq.com>
2018-12-06 19:48:45 +0800

    滚动

--- src/views/CourseWareDetail.vue
+++ src/views/CourseWareDetail.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="course-ware">
-    {{currentPage}} / {{pageCount}}
+    <!--{{currentPage}} / {{pageCount}}-->
+    <!--<pdf-->
+    <!--v-if="src"-->
+    <!--:src="baseUrl"-->
+    <!--@num-pages="pageCount = $event"-->
+    <!--@page-loaded="currentPage = $event"-->
+    <!--@progress="progress = $event"></pdf>-->
     <pdf
-    v-if="src"
-    :src="baseUrl"
-    @num-pages="pageCount = $event"
-    @page-loaded="currentPage = $event"></pdf>
+      v-for="i in numPages"
+      :key="i"
+      :src="src"
+      :page="i"
+    ></pdf>
   </div>
 </template>
 
@@ -16,18 +23,33 @@
     name: 'courseWareDetail',
     data() {
       return {
-        src: '',
-        currentPage: 0,
-        pageCount: 0,
+        // src: loadingTask,
+        // currentPage: 0,
+        // pageCount: 0,
+        // progress: 0,
+        src: null,
+        numPages: undefined,
       }
     },
+    watch: {
+    },
     computed: {
-      baseUrl() {
-        return window.config.baseUrl + '/static/courseware/pdf/' + this.$route.query.path;
-      },
+      // baseUrl() {
+      //   return window.config.baseUrl + '/static/courseware/pdf/' + this.$route.query.path;
+      // },
     },
     created() {
-      this.src = this.$route.query.path;
+      this.src = pdf.createLoadingTask(`${window.config.baseUrl}/static/courseware/pdf/${this.$route.query.path}`);
+      // this.$nextTick(() => {
+      //   this.src = this.$route.query.path;
+      // });
+    },
+    methods: {
+    },
+    beforeMount() {
+      this.src.then(pdf => {
+        this.numPages = pdf.numPages;
+      });
     },
     components: {
       pdf
